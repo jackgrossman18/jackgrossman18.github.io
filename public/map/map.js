40119b814c3cdd4f1c0575fdb15bf5b7f21889bb
Jack Grossman <jackgrossman@Jacks-MacBook-Pro.local>
2018-07-26 17:31:37 -0400

    Transfering portfolio to git repository

---
+++ public/map/map.js
@@ -0,0 +1,10 @@
+mapboxgl.accessToken =
+  "pk.eyJ1IjoiamFja2dyb3NzbWFuIiwiYSI6ImNpbWZqeG1hMjAxcHl2Y202cmhlZGRjYXcifQ.1-so8LElW5dTGT5o941u1w";
+var map = new mapboxgl.Map({
+  container: "map",
+  style: "mapbox://styles/jackgrossman/cjj7mbm7l2i8s2so57twzfqb9",
+  pitch: 0,
+  bearing: 0,
+  zoom: 12,
+  center: [-74.56, 39.32]
+});
