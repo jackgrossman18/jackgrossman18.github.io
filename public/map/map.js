798482bbb4151a904891ed6620b47322fd3cde9c
Jack Grossman <jackgrossman@Jacks-MacBook-Pro.local>
2018-07-27 09:03:15 -0400

    Working Mapbox Map

--- public/map/map.js
+++ public/map/map.js
@@ -2,7 +2,7 @@ mapboxgl.accessToken =
   "pk.eyJ1IjoiamFja2dyb3NzbWFuIiwiYSI6ImNpbWZqeG1hMjAxcHl2Y202cmhlZGRjYXcifQ.1-so8LElW5dTGT5o941u1w";
 var map = new mapboxgl.Map({
   container: "map",
-  style: "mapbox://styles/jackgrossman/cjj7mbm7l2i8s2so57twzfqb9",
+  style: "mapbox://styles/jackgrossman/cjj7qb86l1r732rjrmdh31r70",
   pitch: 0,
   bearing: 0,
   zoom: 12,
