1c0e20240fadfcbff120edf5ce1f36927864aea0
Jack Grossman <jackgrossman@Jacks-MacBook-Pro.local>
2018-08-06 15:42:09 -0400

    Replaced map with working version

--- public/map/map.js
+++ public/map/map.js
@@ -2,7 +2,7 @@ mapboxgl.accessToken =
   "pk.eyJ1IjoiamFja2dyb3NzbWFuIiwiYSI6ImNpbWZqeG1hMjAxcHl2Y202cmhlZGRjYXcifQ.1-so8LElW5dTGT5o941u1w";
 var map = new mapboxgl.Map({
   container: "map",
-  style: "mapbox://styles/jackgrossman/cjj7qb86l1r732rjrmdh31r70",
+  style: "mapbox://styles/jackgrossman/cjkim8kfl09c12rpb7q96uvm7",
   pitch: 0,
   control: "topleft",
   bearing: 0,
